44ec666052abcb8893ef121041a45d599e740793
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-02-03 10:35:10 +0300

    feat: add request and error loggers

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const { createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const errorMiddleware = require('./middlewares/error');
 const { validateLogin, validateRegister } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000, PATH_MONGO = 'mongodb://127.0.0.1:27017/mestodbbbb' } = process.env;
@@ -24,6 +25,8 @@ mongoose.connect(PATH_MONGO, {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateRegister, createUser);
 
@@ -35,6 +38,7 @@ app.use((req, res) => {
   res.status(NOT_FOUND).send({ message: 'Такой страницы не существует.' });
 });
 
+app.use(errorLogger);
 app.use(errors()); // обработчик ошибок celebrate
 app.use(errorMiddleware); // централизованный обработчик ошибок
 
